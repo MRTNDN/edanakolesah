8d1fbae4ac1e1fb8d6c518707f493b37c13cafdb
Andrey <100sss100sss@gmail.com>
2023-12-14 19:53:43 +0500

    poisk + redact coda

--- app/src/main/java/com/westernyey/edanakolesah/main/Main.java
+++ app/src/main/java/com/westernyey/edanakolesah/main/Main.java
@@ -1,7 +1,35 @@
 package com.westernyey.edanakolesah.main;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.content.Intent;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.westernyey.edanakolesah.R;
 
 
-public class Main extends AppCompatActivity {}
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        // Находим RelativeLayout по его идентификатору
+        RelativeLayout relativeLayout = findViewById(R.id.RELA);
+
+        // Добавляем обработчик событий OnClickListener
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Создаем Intent для запуска новой активности
+                Intent intent = new Intent(Main.this, com.westernyey.edanakolesah.main.Restaurant.class);
+                // Запускаем новую активность
+                startActivity(intent);
+            }
+        });
+    }
+}
