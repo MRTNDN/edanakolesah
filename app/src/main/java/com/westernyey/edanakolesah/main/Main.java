4ef2982f57887d16e7428cf716bfb3304e0a9f5d
Andrey <100sss100sss@gmail.com>
2023-12-28 21:31:31 +0500

    + фрагменты под приколы

--- app/src/main/java/com/westernyey/edanakolesah/main/Main.java
+++ app/src/main/java/com/westernyey/edanakolesah/main/Main.java
@@ -4,12 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
-import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.westernyey.edanakolesah.R;
-
 
 public class Main extends AppCompatActivity {
 
@@ -20,13 +18,37 @@ public class Main extends AppCompatActivity {
 
         // Находим RelativeLayout по его идентификатору
         RelativeLayout relativeLayout = findViewById(R.id.RELA);
+        RelativeLayout relativeLayoutp = findViewById(R.id.RELAP);
+        RelativeLayout relativeLayoutr = findViewById(R.id.RELAr);
 
-        // Добавляем обработчик событий OnClickListener
+        // Добавляем обработчик событий OnClickListener для первого RelativeLayout
         relativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Создаем Intent для запуска новой активности
-                Intent intent = new Intent(Main.this, com.westernyey.edanakolesah.main.shopmenu.class);
+                // Создаем Intent для запуска активности shopmenu
+                Intent intent = new Intent(Main.this, shopmenu.class);
+                // Запускаем новую активность
+                startActivity(intent);
+            }
+        });
+
+        // Добавляем обработчик событий OnClickListener для второго RelativeLayout
+        relativeLayoutp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Создаем Intent для запуска активности shopmenu
+                Intent intent = new Intent(Main.this, shopmenu.class);
+                // Запускаем новую активность
+                startActivity(intent);
+            }
+        });
+
+        // Добавляем обработчик событий OnClickListener для третьего RelativeLayout
+        relativeLayoutr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Создаем Intent для запуска активности shopmenu
+                Intent intent = new Intent(Main.this, shopmenu.class);
                 // Запускаем новую активность
                 startActivity(intent);
             }
--- app/src/main/java/com/westernyey/edanakolesah/main/shopmenu.java
+++ app/src/main/java/com/westernyey/edanakolesah/main/shopmenu.java
@@ -1,15 +1,19 @@
 package com.westernyey.edanakolesah.main;
 
+import static android.text.TextUtils.replace;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
 import com.westernyey.edanakolesah.R;
+
+import java.sql.Connection;
 
 public class shopmenu extends FragmentActivity {
     @Override
@@ -20,16 +24,26 @@ public class shopmenu extends FragmentActivity {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_search_up, new fragment_search())
-                    .replace(R.id.fragment_content_container, new ContentFragment())
-                    .replace(R.id.fragment_buttons_container, new ButtonsFragment())
+                    .replace(R.id.fragment_content_container, new ())
+                    .replace(R.id.fragment_buttons_container, new Fragment())
+                    .commit();
+
+            // Создаем экземпляр ButtonsFragment
+            ButtonsFragment buttonsFragment = new ButtonsFragment();
+
+            // Добавляем ButtonsFragment в контейнер
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_buttons_container, buttonsFragment)
                     .commit();
         }
+    }
+
+    private Connection replace(int fragmentButtonsContainer, Fragment fragment) {
     }
 
     public static class fragment_search extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            // Ваши действия по созданию вида для фрагмента fragment_search
             return inflater.inflate(R.layout.fragment_search, container, false);
         }
     }
@@ -37,7 +51,6 @@ public class shopmenu extends FragmentActivity {
     public static class ContentFragment extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            // Ваши действия по созданию вида для фрагмента ContentFragment
             return inflater.inflate(R.layout.stocks, container, false);
         }
     }
@@ -45,8 +58,51 @@ public class shopmenu extends FragmentActivity {
     public static class ButtonsFragment extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            // Ваши действия по созданию вида для фрагмента ButtonsFragment
-            return inflater.inflate(R.layout.fragment_buttons, container, false);
+            View view = inflater.inflate(R.layout.main, container, false);
+
+            // Находим кнопки по их идентификаторам
+            Button buttonRular = view.findViewById(R.id.RELAr);
+            Button buttonRelap = view.findViewById(R.id.RELAP);
+            Button buttonRela = view.findViewById(R.id.RELA);
+
+            buttonRular.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    replaceContentFragment(new RestaurantFragment());
+                }
+            });
+
+            buttonRelap.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    replaceContentFragment(new ProductFragment());
+                }
+            });
+
+            return view;
         }
+
+        private void replaceContentFragment(Fragment fragment) {
+            AppCompatActivity activity = (AppCompatActivity) getActivity();
+            if (activity != null) {
+                activity.getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_content_container, fragment)
+                        .commit();
+            }
+        }
+    }
+}
+
+class RestaurantFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.restaurant_fragment, container, false);
+    }
+}
+
+class ProductFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.product_fragment, container, false);
     }
 }
